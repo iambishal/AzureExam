e119508c4d0cd03701e51047225683952b7f7db2
Bishal Shrestha <Az204Prep2022@gmail.com>
2023-02-13 15:17:41 -0600

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PathologyApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
